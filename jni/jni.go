d3dc208237694f2189aae16fdce3b36bad04d19e
Elias Naur <mail@eliasnaur.com>
2021-01-25 11:09:28 +0100

    jni: move package documentation to the package declaration
    Updates tailscale/tailscale#1195
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- jni/jni.go
+++ jni/jni.go
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package jni
-
 // Package jni implements various helper functions for communicating with the Android JVM
 // though JNI.
+package jni
 
 import (
 	"errors"
