27283a8bb0a09387dead4d3aa2b7e3e43bb1a10c
Denton Gentry <dgentry@tailscale.com>
2021-07-15 21:27:40 -0700

    Update for 1.11 unstable release.
    Use OSS version in tailscale-version.sh
    
    Signed-off-by: Denton Gentry <dgentry@tailscale.com>

--- version/tailscale-version.sh
+++ version/tailscale-version.sh
@@ -19,22 +19,24 @@ if [ -z "$mod_version" ]; then
 fi
 
 case "$mod_version" in
-	*-*-*)
-		# A pseudo-version such as "v1.1.1-0.20201030135043-eab6e9ea4e45"
-		# includes the commit hash.
-		mod_version=${mod_version##*-*-}
-		;;
+        *-*-*)
+                # A pseudo-version such as "v1.1.1-0.20201030135043-eab6e9ea4e45"
+                # includes the commit hash.
+                mod_version=${mod_version##*-*-}
+                ;;
 esac
 
-tailscale_clone=/tmp/tailscale-clone
-
-if [ ! -d "$tailscale_clone" ]; then
-       git clone -q https://github.com/tailscale/tailscale.git "$tailscale_clone"
-fi
+tailscale_clone=$(mktemp -d -t tailscale-clone-XXXXXXXXXX)
+git clone -q https://github.com/tailscale/tailscale.git "$tailscale_clone"
 
 cd $tailscale_clone
 git reset --hard -q
 git clean -d -x -f
 git fetch -q --all --tags
 git checkout -q "$mod_version"
-echo $mod_version-`git describe --always --exclude "*" --dirty --abbrev=$1`
+
+eval $(./build_dist.sh shellvars)
+tailscale_version=${VERSION_SHORT}
+
+echo ${tailscale_version}-`git describe --always --exclude "*" --dirty --abbrev=$1`
+rm -rf "$tailscale_clone"
