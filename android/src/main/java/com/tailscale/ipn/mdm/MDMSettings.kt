de6b92242cbcb46ca9d212d08d67d672ef2aaa5c
Andrea Gottardo <andrea@gottardo.me>
2024-03-08 15:19:50 -0800

    mdm: implement initial data structure to read from Android RestrictionsManager
    updates ENG-2849
    
    Implements the basic data model for supporting MDM to allow us to add the hooks in the UI.
    
    Signed-off-by: Jonathan Nobels <jonathan@tailscale.com>

--- android/src/main/java/com/tailscale/ipn/mdm/MDMSettings.kt
+++ android/src/main/java/com/tailscale/ipn/mdm/MDMSettings.kt
@@ -1,10 +1,34 @@
 package com.tailscale.ipn.mdm
 
-enum class NetworkDevices(val value: String) {
-    currentUser ("current-user"),
-    otherUsers ("other-users"),
-    taggedDevices ("tagged-devices"),
-}
-class MDMSettings {
+import android.content.RestrictionsManager
+import com.tailscale.ipn.App
+
+class MDMSettings(private val restrictionsManager: RestrictionsManager? = null) {
+    // TODO(angott): implement a typed enum string array type
     val hiddenNetworkDevices: List<NetworkDevices> = emptyList()
+
+    fun get(setting: BooleanSetting): Boolean {
+        restrictionsManager?.let  { restrictionsManager ->
+            restrictionsManager.applicationRestrictions.containsKey(setting.key)
+            return restrictionsManager.applicationRestrictions.getBoolean(setting.key)
+        } ?: run {
+            return App.getApplication().encryptedPrefs.getBoolean(setting.key, false)
+        }
+    }
+
+    fun get(setting: StringSetting): String? {
+        return App.getApplication().encryptedPrefs.getString(setting.key, null)
+    }
+
+    fun get(setting: AlwaysNeverUserDecidesSetting): AlwaysNeverUserDecidesValue {
+        val storedString = App.getApplication().encryptedPrefs.getString(setting.key, "user-decides")
+                ?: "user-decides"
+        return AlwaysNeverUserDecidesValue.valueOf(storedString)
+    }
+
+    fun get(setting: ShowHideSetting): ShowHideValue {
+        val storedString = App.getApplication().encryptedPrefs.getString(setting.key, "show")
+                ?: "show"
+        return ShowHideValue.valueOf(storedString)
+    }
 }
---
+++ android/src/main/java/com/tailscale/ipn/mdm/MDMSettingsDefinitions.kt
@@ -0,0 +1,52 @@
+package com.tailscale.ipn.mdm
+
+enum class BooleanSetting(val key: String, val localizedTitle: String) {
+    ForceEnabled("ForceEnabled", "Force Enabled Connection Toggle")
+}
+
+enum class StringSetting(val key: String, val localizedTitle: String) {
+    ExitNodeID("ExitNodeID", "Forced Exit Node: Stable ID"),
+    KeyExpirationNotice("KeyExpirationNotice", "Key Expiration Notice Period"),
+    LoginURL("LoginURL", "Custom control server URL"),
+    ManagedByCaption("ManagedByCaption", "Managed By - Caption"),
+    ManagedByOrganizationName("ManagedByOrganizationName", "Managed By - Organization Name"),
+    ManagedByURL("ManagedByURL", "Managed By - Support URL"),
+    Tailnet("Tailnet", "Recommended/Required Tailnet Name"),
+}
+
+// A setting representing a String value which is set to either `always`, `never` or `user-decides`.
+enum class AlwaysNeverUserDecidesSetting(val key: String, val localizedTitle: String) {
+    AllowIncomingConnections("AllowIncomingConnections", "Allow Incoming Connections"),
+    DetectThirdPartyAppConflicts("DetectThirdPartyAppConflicts", "Detect potentially problematic third-party apps"),
+    ExitNodeAllowLANAccess("ExitNodeAllowLANAccess", "Allow LAN Access when using an exit node"),
+    PostureChecking("PostureChecking", "Enable Posture Checking"),
+    UseTailscaleDNSSettings("UseTailscaleDNSSettings", "Use Tailscale DNS Settings"),
+    UseTailscaleSubnets("UseTailscaleSubnets", "Use Tailscale Subnets")
+}
+
+enum class AlwaysNeverUserDecidesValue(val value: String) {
+    Always("always"),
+    Never("never"),
+    UserDecides("user-decides")
+}
+
+// A setting representing a String value which is set to either `show` or `hide`.
+enum class ShowHideSetting(val key: String, val localizedTitle: String) {
+    ExitNodesPicker("ExitNodesPicker", "Exit Nodes Picker"),
+    ManageTailnetLock("ManageTailnetLock", "“Manage Tailnet lock” menu item"),
+    ResetToDefaults("ResetToDefaults", "“Reset to Defaults” menu item"),
+    RunExitNode("RunExitNode", "Run as Exit Node"),
+    TestMenu("TestMenu", "Show Debug Menu"),
+    UpdateMenu("UpdateMenu", "“Update Available” menu item"),
+}
+
+enum class ShowHideValue(val value: String) {
+    Show("show"),
+    Hide("hide")
+}
+
+enum class NetworkDevices(val value: String) {
+    currentUser("current-user"),
+    otherUsers("other-users"),
+    taggedDevices("tagged-devices"),
+}
