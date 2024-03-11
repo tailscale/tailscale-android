9aa241b76a495d0b03a9c9f1f9dba63176f831ed
Jonathan Nobels <jonathan@tailscale.com>
2024-03-11 10:52:39 -0400

    android: complete settings implementation
    updates ENG-2854
    
    Render the different types of settings.  CorpDNS is working and the about and bug report are stubbed to TBD views.  Error handling not implemented (though we may choose to just swallow API errors here similar to iOS).  Future-proofed to allow us to add any setting we wish to this view.
    
    Styling incomplete.
    
    Corrected the bit mask for the notifier which was off by one bit.
    
    Signed-off-by: Jonathan Nobels <jonathan@tailscale.com>

--- android/src/main/java/com/tailscale/ipn/ui/viewModel/SettingsViewModel.kt
+++ android/src/main/java/com/tailscale/ipn/ui/viewModel/SettingsViewModel.kt
@@ -10,18 +10,36 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tailscale.ipn.ui.service.IpnActions
 import com.tailscale.ipn.ui.service.IpnModel
+import com.tailscale.ipn.ui.service.toggleCorpDNS
 import com.tailscale.ipn.ui.view.SettingsNav
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 enum class SettingType { NAV, SWITCH, NAV_WITH_TEXT }
 
+// Represents a UI setting.
+// title: The title of the setting
+// type: The type of setting
+// enabled: Whether the setting is enabled
+// value: The value of the setting for textual settings
+// isOn: The value of the setting for switch settings
+// onClick: The action to take when the setting is clicked (typicall for navigation)
+// onToggle: The action to take when the setting is toggled (typically for switches)
+//
+// Behavior is undefined if you mix the types here. Switch settings should supply an
+// isOn and onToggle, while navigation settings should supply an onClick and an optional
+// value
 data class Setting(
         val title: String,
         val type: SettingType,
-        val value: String = "",
-        val isOn: Boolean = false,
-        val onClick: () -> Unit = {})
+        val enabled: MutableStateFlow<Boolean> = MutableStateFlow(false),
+        val value: MutableStateFlow<String?>? = null,
+        val isOn: MutableStateFlow<Boolean?>? = null,
+        val onClick: () -> Unit = {},
+        val onToggle: (Boolean) -> Unit = {})
 
 data class SettingBundle(val title: String? = null, val settings: List<Setting>)
 
@@ -34,13 +52,32 @@ class SettingsViewModel(val model: IpnModel, val ipnActions: IpnActions, val nav
     val tailnetName = user?.Name ?: ""
     val isAdmin = model.netmap.value?.SelfNode?.isAdmin ?: false
 
+    val useDNSSetting = Setting(
+            "Use Tailscale DNS",
+            SettingType.SWITCH,
+            isOn = MutableStateFlow(model.prefs.value?.CorpDNS),
+            onToggle = { model.toggleCorpDNS {
+                // (jonathan) TODO: Error handling
+            } })
+
+    init {
+        viewModelScope.launch {
+            // Monitor our prefs for changes and update the displayed values accordingly
+            model.prefs.collect { prefs ->
+                useDNSSetting.isOn?.value = prefs?.CorpDNS
+                useDNSSetting.enabled?.value = prefs != null
+            }
+        }
+    }
+
     val settings: List<SettingBundle> = listOf(
             SettingBundle(settings = listOf(
-                    Setting("Use Tailscale DNS", SettingType.SWITCH),
+                    useDNSSetting,
             )),
+            // General settings, always enabled
             SettingBundle(settings = listOf(
-                Setting("About", SettingType.NAV, onClick = { navigation.onNavigateToAbout()}),
-                Setting("Bug Report", SettingType.NAV, onClick = {  navigation.onNavigateToBugReport()})
+                Setting("About", SettingType.NAV, onClick = { navigation.onNavigateToAbout()}, enabled = MutableStateFlow(true)),
+                Setting("Bug Report", SettingType.NAV, onClick = {  navigation.onNavigateToBugReport()}, enabled = MutableStateFlow(true))
             ))
     )
 
