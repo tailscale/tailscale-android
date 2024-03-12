cffb7d98924f89e9cb4d41960c90898daa5dd249
Jonathan Nobels <jonathan@tailscale.com>
2024-03-12 11:24:30 -0400

    android: add missing license headers
    updates ENG-2859
    
    Signed-off-by: Jonathan Nobels <jonathan@tailscale.com>

--- android/src/main/java/com/tailscale/ipn/ui/view/BugReportView.kt
+++ android/src/main/java/com/tailscale/ipn/ui/view/BugReportView.kt
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 Tailscale Inc & AUTHORS All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package com.tailscale.ipn.ui.view
 
 import androidx.compose.foundation.clickable
