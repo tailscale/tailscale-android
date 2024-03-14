11eead245202488a94f69b5b4cbebbca634e54f8
Jonathan Nobels <jonathan@tailscale.com>
2024-03-14 10:26:21 -0400

    android: move the composablestringformatter to it's own file
    Updates tailscale/corp#18202
    
    This class deserves it's own file and some documentation
    
    Signed-off-by: Jonathan Nobels <jonathan@tailscale.com>

---
+++ android/src/main/java/com/tailscale/ipn/ui/util/ComposableStringFormatter.kt
@@ -0,0 +1,21 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+package com.tailscale.ipn.ui.util
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.tailscale.ipn.R
+
+
+// Convenience wrapper for passing formatted strings to Composables
+class ComposableStringFormatter(@StringRes val stringRes: Int = R.string.template, private vararg val params: Any) {
+
+    // Convenience constructor for passing a non-formatted string directly
+    constructor(string: String) : this(stringRes = R.string.template, string)
+
+    // Returns the fully formatted string
+    @Composable
+    fun getString(): String = stringResource(id = stringRes, *params)
+}
