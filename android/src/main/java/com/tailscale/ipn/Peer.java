facf6406c3dd3bde171506796f311b4fb907bb67
kari-ts <135075563+kari-ts@users.noreply.github.com>
2024-03-27 16:49:29 -0700

    Clean up Google sign in (#258)
    This is unnecessary; we are just using browser login
    
    Updates tailscale/corp#18202
    
    Signed-off-by: kari-ts <kari@tailscale.com>

--- android/src/main/java/com/tailscale/ipn/Peer.java
+++ android/src/main/java/com/tailscale/ipn/Peer.java
@@ -11,27 +11,18 @@ public class Peer extends Fragment {
     private static int resultOK = -1;
 
     public class RequestCodes {
-        public static final int requestSignin = 1000;
         public static final int requestPrepareVPN = 1001;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case RequestCodes.requestSignin:
-                if (resultCode != resultOK) {
-                    // TODO: send null Google token
-                    break;
-                }
-                // TODO: send Google token
-            case RequestCodes.requestPrepareVPN:
-                if (resultCode == resultOK) {
-                    App.getApplication().startVPN();
-                } else {
-                    App.getApplication().setWantRunning(false);
-                    // notify VPN revoked
-                }
-
+        if (requestCode == RequestCodes.requestPrepareVPN) {
+            if (resultCode == resultOK) {
+                App.getApplication().startVPN();
+            } else {
+                App.getApplication().setWantRunning(false);
+                // notify VPN revoked
+            }
         }
     }
 }
