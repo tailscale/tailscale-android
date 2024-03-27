44ba20a24e75583ce58652332ba0fa77da9690f9
Percy Wegmann <percy@tailscale.com>
2024-03-27 08:51:34 -0500

    android/ui: prompt for permissions and show list of permissions with statuses
    Updates #ENG-2948
    
    Signed-off-by: Percy Wegmann <percy@tailscale.com>

---
+++ android/src/main/java/com/tailscale/ipn/ui/view/PermissionsView.kt
@@ -0,0 +1,75 @@
+// Copyright (c) Tailscale Inc & AUTHORS
+// SPDX-License-Identifier: BSD-3-Clause
+
+package com.tailscale.ipn.ui.view
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.tailscale.ipn.R
+import com.tailscale.ipn.ui.model.Permissions
+import com.tailscale.ipn.ui.util.itemsWithDividers
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun PermissionsView(nav: BackNavigation, openApplicationSettings: () -> Unit) {
+  Scaffold(topBar = { Header(title = R.string.permissions, onBack = nav.onBack) }) { innerPadding ->
+    val permissions = Permissions.all
+    val permissionStates =
+        rememberMultiplePermissionsState(permissions = permissions.map { it.name })
+    val permissionsWithStates = permissions.zip(permissionStates.permissions)
+
+    LazyColumn(modifier = Modifier.padding(innerPadding)) {
+      itemsWithDividers(permissionsWithStates) { (permission, state) ->
+        var modifier: Modifier = Modifier
+        if (!state.status.isGranted) {
+          modifier = modifier.clickable { openApplicationSettings() }
+        }
+        ListItem(
+            modifier = modifier,
+            leadingContent = {
+              Icon(
+                  if (state.status.isGranted) Icons.Filled.CheckCircle else Icons.Filled.Warning,
+                  modifier = Modifier.size(24.dp),
+                  contentDescription =
+                      stringResource(if (state.status.isGranted) R.string.ok else R.string.warning))
+            },
+            headlineContent = {
+              Text(stringResource(permission.title), style = MaterialTheme.typography.titleMedium)
+            },
+            supportingContent = {
+              Text(
+                  stringResource(
+                      if (state.status.isGranted) permission.grantedDescription
+                      else permission.neededDescription))
+            },
+            trailingContent = {
+              if (!state.status.isGranted) {
+                Icon(
+                    Icons.AutoMirrored.Outlined.KeyboardArrowRight,
+                    modifier = Modifier.size(24.dp),
+                    contentDescription = stringResource(R.string.more))
+              }
+            },
+        )
+      }
+    }
+  }
+}
