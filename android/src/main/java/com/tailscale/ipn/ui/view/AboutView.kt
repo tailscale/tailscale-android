ef894fa8ca0ce559481ab0df8dd10e7c1803a7fe
Percy Wegmann <percy@tailscale.com>
2024-04-03 17:09:55 -0500

    android/ui: new icon for about screen
    Updates tailscale/corp#18202
    
    Signed-off-by: Percy Wegmann <percy@tailscale.com>

--- android/src/main/java/com/tailscale/ipn/ui/view/AboutView.kt
+++ android/src/main/java/com/tailscale/ipn/ui/view/AboutView.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -43,9 +42,9 @@ fun AboutView(nav: BackNavigation) {
                   Modifier.width(100.dp)
                       .height(100.dp)
                       .clip(RoundedCornerShape(50))
-                      .background(Color.Black)
+                      .background(MaterialTheme.colorScheme.onSurface)
                       .padding(15.dp),
-              painter = painterResource(id = R.drawable.ic_tile),
+              painter = painterResource(id = R.drawable.androidicon),
               contentDescription = stringResource(R.string.app_icon_content_description))
 
           Column(
